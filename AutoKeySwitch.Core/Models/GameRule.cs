25bec0d2caf851f5527f6c834a1f10df2d6ee372
BertTristan <tristan.bert@free.fr>
2026-01-07 10:22:05 +0100

    ✨ feat: Add GameRule and RulesConfig models

---
+++ AutoKeySwitch.Core/Models/GameRule.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AutoKeySwitch.Core.Models
+{
+    public class GameRule
+    {
+        public string AppName { get; set; } = "";
+        public string AppPath { get; set; } = "";
+        public string Layout { get; set; } = "";
+        public bool Enabled { get; set; } = false;
+
+    }
+}
