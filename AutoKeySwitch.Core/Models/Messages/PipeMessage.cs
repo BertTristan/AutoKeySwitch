f712f685fc0d9d65f1c9b900ff80dbcc03a4339c
BertTristan <tristan.bert@free.fr>
2026-01-07 10:42:41 +0100

    ✨ feat: Implement named pipe communication

---
+++ AutoKeySwitch.Core/Models/Messages/PipeMessage.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AutoKeySwitch.Core.Models.Messages
+{
+    public abstract class PipeMessage
+    {
+        public abstract string Type { get; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
