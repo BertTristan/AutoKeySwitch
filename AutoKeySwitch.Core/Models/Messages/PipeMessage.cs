f982c7a9cfce364433d84f408afdfe712043ce59
Tristan Bert <147594893+BertTristan@users.noreply.github.com>
2026-01-20 18:16:51 +0100

    🔥 refactor: remove Core project

--- AutoKeySwitch.Core/Models/Messages/PipeMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace AutoKeySwitch.Core.Models.Messages
-{
-    public abstract class PipeMessage
-    {
-        public abstract string Type { get; }
-        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-    }
-}
--- AutoKeySwitch.Core/Models/RulesConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace AutoKeySwitch.Core.Models
-{
-    public class RulesConfig
-    {
-        public string DefaultLayout { get; set; } = "fr-FR";
-        public List<GameRule> Rules { get; set; } = [];
-    }
-}
