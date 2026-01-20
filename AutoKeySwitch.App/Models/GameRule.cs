190d53358c855e313e7df8ddfc40558234e3da37
KoDiian <tristan.bert@free.fr>
2026-01-20 16:47:00 +0100

    ♻️ refactor: move Core models to App

---
+++ AutoKeySwitch.App/Models/GameRule.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AutoKeySwitch.App.Models
+{
+    public class GameRule
+    {
+        public string AppName { get; set; } = "";
+        public string AppPath { get; set; } = "";
+        public string Layout { get; set; } = "";
+        public bool Enabled { get; set; } = false;
+
+    }
+}
---
+++ AutoKeySwitch.App/Models/RulesConfig.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AutoKeySwitch.App.Models
+{
+    public class RulesConfig
+    {
+        public string DefaultLayout { get; set; } = "fr-FR";
+        public List<GameRule> Rules { get; set; } = [];
+    }
+}
